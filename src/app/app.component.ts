97d9cdea999ce81b9596972f9054c9d276184ce0
Zouari Bassem <bassemzouari.1994@gmail.com>
2018-11-16 10:30:23 +0100

    Ex2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Article } from './models/article';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'app';
+  listArticle: Article[]
+  ngOnInit() {
+    this.listArticle = [
+      { titre: "Le champion du monde", auteur: "Med Taher", contenu: "le champion du mode", date: "12/12/2005" },
+      { titre: "Le champion du monde", auteur: "Med Taher", contenu: "le champion du mode", date: "12/12/2005" },
+      { titre: "Le champion du monde", auteur: "Med Taher", contenu: "le champion du mode", date: "12/12/2005" },
+      { titre: "Le champion du monde", auteur: "Med Taher", contenu: "le champion du mode", date: "12/12/2005" }
+    ]
+  }
 }
