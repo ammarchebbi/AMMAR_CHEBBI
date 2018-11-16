e464bff42191990a05569233bf26741546c76dfb
Zouari Bassem <bassemzouari.1994@gmail.com>
2018-11-16 10:24:27 +0100

    Ex2

--- src/app/Components/estimation-cout/estimation-cout.component.ts
+++ src/app/Components/estimation-cout/estimation-cout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Position } from '../../models/position';
 
 @Component({
   selector: 'app-estimation-cout',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EstimationCoutComponent implements OnInit {
 
-  constructor() { }
+  position: Position;
+  nbre: number;
+  options: Position[];
+  cout: number;
+  constructor() { this.position = new Position() }
 
   ngOnInit() {
+
+    this.position = new Position();
+    this.cout = 0;
+    this.options = [{ name: "" }, { name: "haut" }, { name: "bas" }];
+    this.nbre = 0;
   }
 
+  calculer() {
+    console.log("calculer");
+    console.log(this.position);
+    if (this.position.name == 'haut') {
+      if (this.nbre <= 30) {
+        this.cout = 10;
+      } else {
+        this.cout = 20;
+      }
+    }
+    else if (this.position.name == 'bas') {
+      if (this.nbre <= 30) {
+        this.cout = 5;
+      } else {
+        this.cout = 15;
+      }
+    }
+  }
 }
